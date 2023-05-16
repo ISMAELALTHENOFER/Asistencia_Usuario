0987f835063f66ad987fff23d35e3ff2fc06b0d9
Ismael Althenofer <ismaelalthenofer85@gmail.com>
2023-05-16 11:32:28 -0300

    Se crea el Header y el Footer

---
+++ view/admin/footer.php
@@ -0,0 +1,26 @@
+  <footer class="main-footer">
+  </footer>
+
+<!-- jQuery 3 -->
+
+  <script src="../../library/js/jquery-3.1.1.min.js"></script>
+    <!-- Bootstrap 3.3.5 -->
+    <script src="../../library/js/bootstrap.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../../library/js/app.min.js"></script>
+
+    <!-- DATATABLES -->
+    <script src="../../library/datatables/jquery.dataTables.min.js"></script>    
+    <script src="../../library/datatables/dataTables.buttons.min.js"></script>
+    <script src="../../library/datatables/buttons.html5.min.js"></script>
+    <script src="../../library/datatables/buttons.colVis.min.js"></script>
+    <script src="../../library/datatables/jszip.min.js"></script>
+    <script src="../../library/datatables/pdfmake.min.js"></script>
+    <script src="../../library/datatables/vfs_fonts.js"></script> 
+
+    <script src="../../library/js/bootbox.min.js"></script> 
+    <script src="../../library/js/bootstrap-select.min.js"></script>
+    <script src="../../library/js/filestyle.min.js"> </script>
+
+</body> 
+</html>

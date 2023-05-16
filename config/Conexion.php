7507a3272bdd2154de807dce579673ccbddfb489
Ismael Althenofer <ismaelalthenofer85@gmail.com>
2023-05-16 11:28:54 -0300

    Modificacion DB

--- config/Conexion.php
+++ config/Conexion.php
@@ -3,7 +3,7 @@
 define("DB_HOST", "127.0.0.1");
 
 // nombre de la base de datos
-define("DB_NAME", "control_asistencia");
+define("DB_NAME", "cc_asistencia");
 
 //nombre de usuario de base de datos
 define("DB_USERNAME", "root");
